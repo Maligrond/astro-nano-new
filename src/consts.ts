16b8c6c9ed301804f2167a21137a79e4c24a2bc5
Maligrond <stormyweatherlight@gmail.com>
2024-12-25 20:45:37 +0100

    newnewnew

--- src/consts.ts
+++ src/consts.ts
@@ -3,28 +3,28 @@ import type { Site, Metadata, Socials } from "@types";
 export const SITE: Site = {
   NAME: "Kaban Blog",
   EMAIL: "stormyweatherlight@gmail.com",
-  NUM_POSTS_ON_HOMEPAGE: 3,
+  NUM_POSTS_ON_HOMEPAGE: 5,
   NUM_WORKS_ON_HOMEPAGE: 0,
   NUM_PROJECTS_ON_HOMEPAGE: 0,
 };
 
 export const HOME: Metadata = {
-  TITLE: "Home",
+  TITLE: "Блог Кабана",
   DESCRIPTION: "Astro Nano is a minimal and lightweight blog and portfolio.",
 };
 
 export const BLOG: Metadata = {
-  TITLE: "Blog",
+  TITLE: "Блог",
   DESCRIPTION: "A collection of articles on topics I am passionate about.",
 };
 
 export const WORK: Metadata = {
-  TITLE: "Work",
+  TITLE: "Работа",
   DESCRIPTION: "Where I have worked and what I have done.",
 };
 
 export const PROJECTS: Metadata = {
-  TITLE: "Projects",
+  TITLE: "Проекты",
   DESCRIPTION: "A collection of my projects, with links to repositories and demos.",
 };
 
